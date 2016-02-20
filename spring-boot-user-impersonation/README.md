03fb5c62d1dff36655865cc404bfbbcb4bef3a75
Christian Claus <ch.claus@me.com>
2016-02-20 13:56:50 +0100

    initial commit for spring-boot-user-impersonation

---
+++ spring-boot-user-impersonation/README.md
@@ -0,0 +1,2 @@
+# spring-boot-pid
+An example project for PID generation with spring-boot. 
---
+++ spring-boot-user-impersonation/src/main/java/de/chclaus/examples/SpringBootUserImpersonationApplication.java
@@ -0,0 +1,12 @@
+package de.chclaus.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserImpersonationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUserImpersonationApplication.class, args);
+	}
+}
