a6c29ee55cb228737e3e8649c7418b1faa16d856
Christian Claus <ch.claus@me.com>
2016-03-16 22:57:55 +0100

    Documentation of the user service.

--- spring-boot-error-handling/src/main/java/de/chclaus/examples/service/user/UserService.java
+++ spring-boot-error-handling/src/main/java/de/chclaus/examples/service/user/UserService.java
@@ -4,11 +4,18 @@ import de.chclaus.examples.controller.user.UserDTO;
 import org.springframework.stereotype.Service;
 
 /**
+ * Simple service to demonstrate exception handling.
+ *
  * @author Christian Claus (ch.claus@me.com)
  */
 @Service
 public class UserService {
 
+  /**
+   * Method to demonstrate a fake user lookup.
+   * @param username The username... There is just an admin account.
+   * @return a DTO representation of an user.
+   */
   public UserDTO findUserByName(String username) {
 
     // We just have an administrator account. Nothing else. ;)
