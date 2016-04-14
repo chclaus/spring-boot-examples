90d1dd1e312fa220c014dd8eedf28b11f1ac23a2
Christian Claus <ch.claus@me.com>
2016-04-14 19:47:57 +0200

    Added example project for spring boot quartz starter.

---
+++ spring-boot-quartz/src/main/java/de/chclaus/examples/SpringBootQuartzApplication.java
@@ -0,0 +1,12 @@
+package de.chclaus.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuartzApplication.class, args);
+	}
+}
